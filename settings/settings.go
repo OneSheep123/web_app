0802e8b45d6a1938980f45c0b6a68d3934958b8d
OneSheep123 <1468254393@qq.com>
2022-12-16 15:28:00 +0800

    feat: 初始化项目环境

---
+++ settings/settings.go
@@ -0,0 +1,72 @@
+package settings
+
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+var Conf = new(AppConfig)
+
+type AppConfig struct {
+	Name         string `mapstructure:"name"`
+	Mode         string `mapstructure:"mode"`
+	Version      string `mapstructure:"version"`
+	Port         int    `mapstructure:"port"`
+	*LogConfig   `mapstructure:"log"`
+	*MySQLConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+
+type LogConfig struct {
+	Level      string `mapstructure:"level"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxAge     int    `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	DbName       string `mapstructure:"db_name"`
+	Port         int    `mapstructure:"port"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Password string `mapstructure:"password"`
+	Port     int    `mapstructure:"port"`
+	DB       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
+func Init() (err error) {
+
+	// 基本上是配合远程配置中心使用的，告诉viper当前的数据使用什么格式去解析
+	//viper.SetConfigType("json")
+
+	viper.SetConfigFile("config.yaml") // 指定配置文件名称（不需要带后缀）
+	viper.AddConfigPath(".")           // 指定查找配置文件的路径（这里使用相对路径）
+	err = viper.ReadInConfig()         // 读取配置信息
+	if err != nil {
+		// 读取配置信息失败
+		return
+	}
+	// 把读取到的配置信息反序列化到 Conf 变量中
+	if err := viper.Unmarshal(Conf); err != nil {
+		fmt.Printf("viper.Unmarshal failed, err:%v\n", err)
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		fmt.Println("配置文件修改了...")
+		if err := viper.Unmarshal(Conf); err != nil {
+			fmt.Printf("viper.Unmarshal failed, err:%v\n", err)
+		}
+	})
+	return
+}
